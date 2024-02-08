05c02db47846fc1f8665b3b41c22e68725e9411a
Grenell <TTGrenell@gmail.com>
2024-02-08 11:43:16 -0500

    Created and implemented TextSizeAdapter spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -19,25 +19,7 @@ class MainActivity : AppCompatActivity() {
         /* Step 1: Populate this array */
         val numberArray = Array(50){i -> (i + 1) * 2}
 
-        with (spinner) {
-            /* Step 2: Create adapter to display items from array in Spinner */
-            adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, numberArray)
-
-
-            // Step 3: Change TextView's text size to the number selected in the Spinner */
-            onItemSelectedListener = object : OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    parent?.run {
-                        displayTextView.textSize = getItemAtPosition(position).toString().toFloat()
-                    }
-                }
-
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-
-                }
-
-            }
-        }
+        spinner.adapter = TextSizeAdapter(this@MainActivity, numberArray)
 
     }
 }
